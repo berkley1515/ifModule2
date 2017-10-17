a615ae08fb8f36c6146036752cff913150d9186d
Berkley Fair <berkley1515@gmail.com>
2017-10-17 16:41:59 -0400

    Add project files.

---
+++ ifModule2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ifModule2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorLabel = new System.Windows.Forms.Label();
+            this.shapeLabel = new System.Windows.Forms.Label();
+            this.colorInput = new System.Windows.Forms.TextBox();
+            this.shapeInput = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.colorLabel.Location = new System.Drawing.Point(26, 40);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(251, 23);
+            this.colorLabel.TabIndex = 0;
+            this.colorLabel.Text = "Enter R for Red, or Enter G for Green";
+            // 
+            // shapeLabel
+            // 
+            this.shapeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.shapeLabel.Location = new System.Drawing.Point(26, 91);
+            this.shapeLabel.Name = "shapeLabel";
+            this.shapeLabel.Size = new System.Drawing.Size(284, 23);
+            this.shapeLabel.TabIndex = 1;
+            this.shapeLabel.Text = "Enter C for Circle, or Enter R for Rectangle";
+            // 
+            // colorInput
+            // 
+            this.colorInput.Location = new System.Drawing.Point(339, 39);
+            this.colorInput.Name = "colorInput";
+            this.colorInput.Size = new System.Drawing.Size(84, 20);
+            this.colorInput.TabIndex = 0;
+            // 
+            // shapeInput
+            // 
+            this.shapeInput.Location = new System.Drawing.Point(339, 90);
+            this.shapeInput.Name = "shapeInput";
+            this.shapeInput.Size = new System.Drawing.Size(84, 20);
+            this.shapeInput.TabIndex = 1;
+            // 
+            // enterButton
+            // 
+            this.enterButton.BackColor = System.Drawing.Color.Silver;
+            this.enterButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGray;
+            this.enterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.enterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.enterButton.Location = new System.Drawing.Point(339, 154);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(84, 36);
+            this.enterButton.TabIndex = 2;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = false;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(464, 361);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.shapeInput);
+            this.Controls.Add(this.colorInput);
+            this.Controls.Add(this.shapeLabel);
+            this.Controls.Add(this.colorLabel);
+            this.Name = "Form1";
+            this.Text = "if Statement 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label colorLabel;
+        private System.Windows.Forms.Label shapeLabel;
+        private System.Windows.Forms.TextBox colorInput;
+        private System.Windows.Forms.TextBox shapeInput;
+        private System.Windows.Forms.Button enterButton;
+    }
+}
+
---
+++ ifModule2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ifModule2
+{
+    public partial class Form1 : Form
+    {
+        //globals
+        string color, shape;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            //holding values
+            color = colorInput.Text;
+            shape = shapeInput.Text;
+
+            Color shapeColor = Color.White; //set white to hold value (otherwise pen/brush gets mad)
+
+            //graphics
+            Graphics sG = this.CreateGraphics();//graphics (AKA. Black Magic!)  (ﾉ◕ヮ◕)⊃━☆*:･ﾟ.* ･｡ﾟ✧
+
+            //pen & brush
+            Pen shapePen = new Pen(shapeColor, 10);
+            SolidBrush shapeBrush = new SolidBrush(shapeColor);
+
+            //colour
+            if (color == "R" | color == "r")
+            {
+                //clearing possible past drawings
+                sG.Clear(shapeColor);
+
+                //set pen/brush colour
+                shapeColor = Color.Red;
+            }
+            else if (color == "G" | color == "g")
+            {
+                //clearing possible past drawings
+                sG.Clear(shapeColor);
+
+                //set pen/brush colour
+                shapeColor = Color.Green;
+            }
+            else
+            {
+                //error! (set value to null)
+                colorInput.Text = null;
+
+                //clearing possible past drawings
+                sG.Clear(shapeColor);
+
+            }
+          
+            //shape
+            if (shape == "C" | shape == "c" | shape == "Circle" | shape == "circle")
+            {
+                //clearing possible past drawings
+                sG.Clear(shapeColor);
+
+                //drawing circle   
+                sG.DrawEllipse(shapePen, 130, 210, 100, 100);//drawing circle
+            }
+            else if (shape == "R" | shape == "r" | shape == "Rectangle" | shape == "rectangle")
+            {
+                //clearing possible past drawings
+                sG.Clear(shapeColor);
+
+                //drawing circle   
+                sG.DrawRectangle(shapePen, 130, 210, 200, 100);//drawing rectangle
+
+            }
+        }
+    }
+}
